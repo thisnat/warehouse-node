73403491868862f1e832bcbfa8930b75e93a3851
Nattawut <thisnattawut@gmail.com>
2021-02-01 19:01:29 +0700

    change package type

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-import express from 'express'
-import cors from 'cors'
+const express = require('express')
+const cors = require('cors')
 
 const app = express();
 app.use(cors());
+app.use(express.urlencoded({extended: true}))
 app.use(express.json());
 
 app.get('/api',(req,res,next) => {
     res.send("hello!")
 });
 
-app.post('/api',(req,res,next) =>{
+app.post('/api',(req,res,next) => {
     const name = req.body.name;
     res.send("hello "+name)
 });
