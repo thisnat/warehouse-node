13fb358ff06cb52c88dff23aa606908015907b29
Nattawut <thisnattawut@gmail.com>
2021-02-02 20:21:39 +0700

    connect to database

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
-const cors = require('cors')
+const cors = require('cors');
+const dbConnection = require('./db');
 
 const app = express();
 app.use(cors());
@@ -12,7 +13,18 @@ app.get('/api',(req,res,next) => {
 
 app.post('/api',(req,res,next) => {
     const name = req.body.name;
-    res.send("hello "+name)
+    res.send(`hello ${name}`)
+});
+
+app.get('/api/products',(req,res,next) => {
+    dbConnection.query('SELECT * from products',(err,result) => {
+        if(err){
+            throw err;
+        }
+        else{
+            res.send(result);
+        }
+    });
 });
 
 app.listen(3001, () => console.log("Server is running at port 3001"))
