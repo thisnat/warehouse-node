4116ffccbcbf97b78e3abdb99eb70a5322ddfbe2
Nattawut <thisnattawut@gmail.com>
2021-02-27 21:27:53 +0700

    update sql file

--- warehouse.sql
+++ warehouse.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 27, 2021 at 02:43 PM
+-- Generation Time: Feb 27, 2021 at 03:27 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.10
 
@@ -41,6 +41,56 @@ CREATE TABLE `cart` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `history`
+--
+
+CREATE TABLE `history` (
+  `id` int(11) NOT NULL,
+  `type` varchar(24) NOT NULL,
+  `status` varchar(24) NOT NULL,
+  `note` varchar(24) DEFAULT 'none',
+  `createDate` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `history`
+--
+
+INSERT INTO `history` (`id`, `type`, `status`, `note`, `createDate`) VALUES
+(73, 'IMPORT', 'ACCEPT', 'none', '2021-02-27 20:41:43'),
+(74, 'IMPORT', 'ACCEPT', 'none', '2021-02-27 20:41:56'),
+(75, 'IMPORT', 'ACCEPT', 'none', '2021-02-27 20:42:25');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `history_item`
+--
+
+CREATE TABLE `history_item` (
+  `id` int(11) NOT NULL,
+  `historyId` int(11) NOT NULL,
+  `productId` int(11) DEFAULT NULL,
+  `name` varchar(24) NOT NULL,
+  `price` float NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `safetyStock` int(11) NOT NULL,
+  `note` text NOT NULL,
+  `createDate` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `history_item`
+--
+
+INSERT INTO `history_item` (`id`, `historyId`, `productId`, `name`, `price`, `quantity`, `safetyStock`, `note`, `createDate`) VALUES
+(71, 73, NULL, 'newza', 23, 1, 1, 'qwrqrw', NULL),
+(72, 74, 23, 'newza', 23, 24, 1, 'qwrqrw', NULL),
+(73, 75, 23, 'newza', 23, 100, 1, 'qwrqrw', NULL);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `products`
 --
 
@@ -67,6 +117,26 @@ INSERT INTO `products` (`id`, `name`, `quantity`, `price`, `safetyStock`, `note`
 (20, 'qerqeor', 123, 324, 1435, 'qwrkjkweporkowept', '2021-02-27 20:08:04'),
 (23, 'newza', 125, 23, 1, 'qwrqrw', '2021-02-27 20:41:48');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(24) NOT NULL,
+  `password` varchar(24) NOT NULL,
+  `role` varchar(24) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `role`) VALUES
+(2, 'admin', 'warehouse', 'admin');
+
 --
 -- Indexes for dumped tables
 --
@@ -78,9 +148,28 @@ ALTER TABLE `cart`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `history`
+--
+ALTER TABLE `history`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `history_item`
+--
+ALTER TABLE `history_item`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `historyId` (`historyId`);
+
+--
 -- Indexes for table `products`
 --
 ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -94,10 +183,38 @@ ALTER TABLE `cart`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=83;
 
 --
+-- AUTO_INCREMENT for table `history`
+--
+ALTER TABLE `history`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=76;
+
+--
+-- AUTO_INCREMENT for table `history_item`
+--
+ALTER TABLE `history_item`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=74;
+
+--
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `history_item`
+--
+ALTER TABLE `history_item`
+  ADD CONSTRAINT `history_item_ibfk_1` FOREIGN KEY (`historyId`) REFERENCES `history` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
