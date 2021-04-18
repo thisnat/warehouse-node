f99dce8a3ad28ea55aaa121a27f4ba6daa857c97
Nattawut <thisnattawut@gmail.com>
2021-04-18 23:44:22 +0700

    add cart remove route

--- routes/CartRouter.js
+++ routes/CartRouter.js
@@ -2,6 +2,7 @@ const express = require("express")
 const router = express.Router()
 const db = require('../db');
 const cart = require('../model/Cart')
+const product = require('../model/Product')
 
 router.get("/",(req,res,next) => {
     cart.getAll(db,(err,result) => {
@@ -59,5 +60,45 @@ router.post("/add",(req,res,next) => {
         }
     });
 });
+
+router.post("/remove/:id",(req,res,next) => {
+    cart.getById(db,req.params.id,(err,result) => {
+        if(err){
+            throw err;
+        }
+        else{
+            product.getById(db,result[0].productId,(err,result2) => {
+                if (result2.length === 0){
+                    // do add product
+                    let data = {
+                        "name": result[0].name, "quantity": result[0].quantity,
+                        "price": result[0].price, "safetyStock": result[0].safetyStock, "note": result[0].note
+                    }
+                    product.create(db,data,(err,result3) => {
+                        if (err){
+                            throw err;
+                        }
+                    })
+                }
+                else{
+                    // do update product
+                    product.update(db, result[0].productId, result[0].quantity + result2[0].quantity, (err, result4) => {
+                        if (err) {
+                            throw err;
+                        }
+                    });
+                }
+                cart.remove(db,req.params.id,(err,result5) => {
+                    if(err){
+                        throw err;
+                    }
+                    else{
+                        res.send(result5);
+                    }
+                });
+            })
+        }
+    });
+})
 
 module.exports = router;
